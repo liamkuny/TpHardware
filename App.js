e736278210d4a0e8d5765c94e695f3948df11096
Liam Kuniewsky <liamkuniewsky@gmail.com>
2023-10-23 20:21:17 -0300

    commit

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import { View, Button, StyleSheet } from 'react-native';
-import { solicitarPermisos, obtenerContactos } from './screens/Contacts';
-import { Weather } from './screens/Weather'; 
-
+import ContactScreen from './screens/Contacts';
+import { Weather } from './screens/Weather';
 function App() {
-  async function manejarObtencionContactos() {
-    await solicitarPermisos();
-    await obtenerContactos();
-    
-  }
-  async function ObtencionClima(){
-    await Weather();
-  }
-  
-
+ 
   return (
     <View style={styles.container}>
-      <Button title="Obtener contactos" onPress={manejarObtencionContactos} />
-      <Button title="Clima" onPress={ObtencionClima} />
+      <Button title="Obtener contactos" onPress={ContactScreen} />
+      <Button title="Clima" onPress={Weather} />
     </View>
   );
 }
--- screens/Contacts.js
+++ screens/Contacts.js
@@ -1,22 +1,47 @@
 
+import React, { useEffect, useState } from 'react';
+import { View, Text , FlatList } from 'react-native';
 import * as Contacts from 'expo-contacts';
 
-async function solicitarPermisos() {
-  const { status } = await Contacts.requestPermissionsAsync();
-  if (status !== 'granted') {
-    alert('Permiso para acceder a los contactos denegado');
-    return;
-  }
+
+const ContactScreen=()=>{
+
+
+const [contacts, setContacts]=useState();
+useEffect(()=>{
+
+  (async()=>{
+    const {status}=await Contacts.requestPermissionsAsync();
+    if(status==='granted')
+    {
+      const {data}=Contacts.getContactsAsync({
+
+        fields:[Contacts.Fields.PhoneNumbers]
+      })
+      if(data.length>0) 
+      {
+        setContacts(data);
+      } 
+    }
+
+  })
+})
+return(
+  <FlatList
+  style={{width:'100%', padding:20, marginTop:50}}
+  data={contacts}
+  keyExtractor={item=>item.id}
+  renderItem={({item})=>{
+  return(
+    <View style={{borderBottomWidth:2}}>
+       <Text style={{fontSize:20, fontWeight: 'bold'}}>{item.name} </Text>
+       <Text style={{fontSize:17}}>, (item.PhoneNumbers && item.PhoneNumbers[0] && item.PhoneNumbers[0].number) </Text>
+    </View>
+  )
+}}
+  />
+
+  
+)
 }
-
-async function obtenerContactos() {
-  const { data } = await Contacts.getContactsAsync({
-    fields: [Contacts.Fields.Name, Contacts.Fields.Emails],
-  });
-
-  if (data.length > 0) {
-    console.log(data);
-  }
-}
-
-export { solicitarPermisos, obtenerContactos };
+export default ContactScreen;
