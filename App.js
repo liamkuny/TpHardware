1ec0b470d554603643aa5e6e872f9371a3f68e0b
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-25 12:03:20 -0300

    commit

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, Button, StyleSheet } from 'react-native';
 import ContactScreen from './screens/Contacts';
+import Weather from './screens/Weather'; // Importa el componente WeatherScreen
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
-function App({ navigation }) {
+function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -24,6 +25,13 @@ function App({ navigation }) {
             headerTitle: 'Contact Screen',
           }}
         />
+        <Stack.Screen
+          name="WeatherScreen"
+          component={Weather} // Agrega WeatherScreen como un componente de pantalla
+          options={{
+            headerTitle: 'Weather Screen',
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -35,6 +43,10 @@ function HomeScreen({ navigation }) {
       <Button
         title="Ir a Contact Screen"
         onPress={() => navigation.navigate('ContactScreen')}
+      />
+      <Button
+        title="Ir a Weather Screen"
+        onPress={() => navigation.navigate('WeatherScreen')}
       />
     </View>
   );
--- screens/Weather.js
+++ screens/Weather.js
@@ -1,69 +1,15 @@
-
 import React, { useEffect, useState } from 'react';
-import { Text, View, ActivityIndicator } from 'react-native';
+import { Text, View, ActivityIndicator, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo';
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const API_KEY = "a96adbe323184895a06145007230610";
 
 const weatherCases = {
-    Rain: {
-        colors: ['#00C6FB', '#005BEA'],
-        title: "Raining like a MF",
-        subtitle: "For more info look outside",
-        icon: 'weather-rainy',
-    },
-    Clear: {
-        colors: ['#FEF253', '#FF7300'],
-        title: "Sunny as heck",
-        subtitle: "Go get yourself some sunshine",
-        icon: 'weather-sunny',
-    },
-    Thunderstorm: {
-        colors: ['#00ECBC', '#007ADF'],
-        title: "Thunderstorm in the area",
-        subtitle: "Actually, outside of the area",
-        icon: 'weather-lightning',
-    },
-    Clouds: {
-        colors: ['#D7D2CC', '#304352'],
-        title: "Cloudy",
-        subtitle: "I know, pretty dull",
-        icon: 'weather-cloudy',
-    },
-    Snow: {
-        colors: ['#7DE2FC', '#B9B6E5'],
-        title: "Cold as ice",
-        subtitle: "Do you want to build a snowman? Probably not.",
-        icon: 'weather-snowy',
-    },
-    Drizzle: {
-        colors: ['#89F7FE', '#66A6FF'],
-        title: "Drizzle",
-        subtitle: "It's like rain, but not really",
-        icon: 'weather-hail',
-    },
-    Smoke: {
-        colors: ['#D7D2CC', '#304352'],
-        title: "Smoke",
-        subtitle: "Smoke Smoke Smoke Smoke",
-        icon: 'weather-fog',
-    },
-    Haze: {
-        colors: ['#D7D2CC', '#304352'],
-        title: "Haze",
-        subtitle: "Haze Haze Haze Haze",
-        icon: 'weather-fog',
-    },
-    Mist: {
-        colors: ['#D7D2CC', '#304352'],
-        title: "Mist",
-        subtitle: "Mist Mist Mist Mist",
-        icon: 'weather-fog',
-    },
+    // ... (tu definición de casos de clima)
 }
 
-async function Weather() {
+function Weather() {
     const [isLoaded, setIsLoaded] = useState(false);
     const [error, setError] = useState(null);
     const [temperature, setTemperature] = useState(null);
@@ -71,27 +17,38 @@ async function Weather() {
     const [country, setCountry] = useState(null);
 
     useEffect(() => {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                getWeather(position.coords.latitude, position.coords.longitude);
-            },
-            (error) => {
-                setError(error);
-            },
-            { enableHighAccuracy: true, timeout: 10000 }
-        );
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    getWeather(position.coords.latitude, position.coords.longitude);
+                },
+                (error) => {
+                    setError(error);
+                },
+                { enableHighAccuracy: true, timeout: 10000 }
+            );
+        } else {
+            setError(new Error("Geolocation is not available."));
+        }
     }, []);
 
     const getWeather = (lat, lon) => {
-        fetch(fetch(`https://api.WeatherApi.com/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`))
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`)
             .then((response) => response.json())
             .then((json) => {
                 setTemperature(json.main.temp);
                 setName(json.weather[0].main);
                 setCountry(json.sys.country);
                 setIsLoaded(true);
+            })
+            .catch((error) => {
+                setError(error);
             });
     };
+
+    const styles = StyleSheet.create({
+        // ... (tus estilos)
+    });
 
     return (
         <View style={styles.container}>
@@ -118,9 +75,5 @@ async function Weather() {
     );
 }
 
-const styles = {
-    // Definición de estilos
-    // ...
-};
+export default Weather;
 
-export {Weather}
