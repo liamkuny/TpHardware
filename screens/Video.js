ed14ec138bf10db0ae5e3dc96ff8e7429e946441
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-08 08:32:42 -0300

    commit

--- screens/Video.js
+++ screens/Video.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, Button } from 'react-native';
+import { View, TextInput, Button} from 'react-native';
 import { Video } from 'expo-av';
-import {AsyncStorage} from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const VideoScreen = () => {
   const [videoUrl, setVideoUrl] = useState(null);
   const [textInputValue, setTextInputValue] = useState('');
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
 
   useEffect(() => {
     AsyncStorage.getItem('lastTextInputValue').then((value) => {
@@ -15,13 +17,17 @@ const VideoScreen = () => {
     });
   }, []);
 
-  const playVideo = () => {
+  const saveTextInputValue = () => {
     setVideoUrl(textInputValue);
+    AsyncStorage.setItem('lastTextInputValue', textInputValue);
   };
 
-  const saveTextInputValue = () => {
-    // Guardar el último texto ingresado en el almacenamiento local
-    AsyncStorage.setItem('lastTextInputValue', textInputValue);
+  const PlayAndPause = () => {
+    if (status.isPlaying) {
+      video.current.pauseAsync();
+    } else {
+      video.current.playAsync();
+    }
   };
 
   return (
@@ -32,18 +38,26 @@ const VideoScreen = () => {
         onChangeText={(text) => setTextInputValue(text)}
       />
       <Button title="Guardar" onPress={saveTextInputValue} />
-      <Button title="Reproducir Video" onPress={playVideo} />
+      <Button title={status.isPlaying ? "Pause" : "Play"} onPress={PlayAndPause} />
 
       {videoUrl && (
         <Video
-          source={{ url: videoUrl }}
-          style={{ width: 300, height: 200 }}
+          source={{ uri: videoUrl }}
+          ref={video}
+          style={{
+            width: 300,
+            height: 200
+          }}
           useNativeControls
           shouldPlay
+          onPlaybackStatusUpdate={status => setStatus(() => status)}
         />
       )}
     </View>
   );
 };
 
+
 export default VideoScreen;
+
+
